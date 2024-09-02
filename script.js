2611d1f0bd5542a60bd316c3896964686c39fc26
Lucas Blessing <lucasblessing@Lucass-MacBook-Air.local>
2024-09-01 19:05:03 -0700

    update html, js, and css file

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 
-$(document).ready(function() {
-	
+$(document).ready(function() {	
 	$("#p1").click(function(){
-  $("#p2").toggle();
+  $("#p2").slideToggle("slow");
+});
+$("#p1").click(function(){
+  $(".GW").slideToggle("slow");
+});
+
+$("#p3").click(function(){
+  $("#p4").slideToggle("slow");
+});
+$("#p3").click(function(){
+  $(".JA").slideToggle("slow");
 });
 });
