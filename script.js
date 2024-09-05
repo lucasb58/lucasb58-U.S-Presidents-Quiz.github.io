ed8fb8ed577495663e7ac711b6b229c1e81ab756
Lucas Blessing <lucasblessing@Lucass-MacBook-Air.local>
2024-09-04 19:31:03 -0700

    added more code

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ $(document).ready(function() {
 		$(this).next().slideToggle("slow");
 	});
 	
+	$(".twentysixth").click(function(){
+		$(this).next().slideToggle("slow");
+	});
+	
+	$(".thirtysecond").click(function(){
+		$(this).next().slideToggle("slow");
+	});
+	
+	$(".thirtyfifth").click(function(){
+		$(this).next().slideToggle("slow");
+	});
+	
 
 	$(".correct").click(function(){
 		$(this).addClass("green");
